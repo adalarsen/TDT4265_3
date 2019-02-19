d01db60382f9106ef511eafecacd4d59b9f90a7d
Ada Skarsholt Larsen <adasl@clab19.idi.ntnu.no>
2019-02-19 13:41:42 +0100

    Task 1 done

--- 1_cnns.py
+++ 1_cnns.py
@@ -30,17 +30,36 @@ class ExampleModel(nn.Module):
                 padding=2
             ),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=num_filters,
+                out_channels=64,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.ReLU()
         )
         # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
-        self.num_output_features = 32*16*16
+        self.num_output_features = 128*4*4
         # Initialize our last fully connected layer
         # Inputs all extracted features from the convolutional layers
         # Outputs num_classes predictions, 1 for each class.
         # There is no need for softmax activation function, as this is
         # included with nn.CrossEntropyLoss
         self.classifier = nn.Sequential(
-            nn.Linear(self.num_output_features, num_classes),
+            nn.Linear(self.num_output_features, 64),
+            nn.Linear(64, num_classes),
         )
 
     def forward(self, x):
@@ -49,11 +68,12 @@ class ExampleModel(nn.Module):
         Args:
             x: Input image, shape: [batch_size, 3, 32, 32]
         """
+        
 
         # Run image through convolutional layers
         x = self.feature_extractor(x)
         # Reshape our input to (batch_size, num_output_features)
-        x = x.view(-1, self.num_output_features)
+        x = x.view(-1, self.num_output_features) # Flatten
         # Forward pass through the fully-connected layers.
         x = self.classifier(x)
         return x
@@ -170,7 +190,7 @@ class Trainer:
 
                 # Gradient descent step
                 self.optimizer.step()
-                
+
                 # Reset all computed gradients to 0
                 self.optimizer.zero_grad()
                  # Compute loss/accuracy for all three datasets.
